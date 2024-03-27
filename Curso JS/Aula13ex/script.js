b2404934abd7b96c03b9b56d0d5603f269397e7d
gustavostb24 <161534757+gustavostb24@users.noreply.github.com>
2024-03-27 00:40:17 +0000

    1234 go

--- Curso JS/Aula13ex/script.js
+++ Curso JS/Aula13ex/script.js
@@ -1,2 +1,12 @@
-//alert('oi')
+function verificar() {
+    let data = new Date() 
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano') //formulário que vai escrever o ano
+    let res = document.getElementById('res') //o resultado do que foi colocado
+    if (fano.value.length == 0 || fano.value > ano) {
+        alert('ERRO, DADOS INVALIDOS PORRA')
+    } else {
+        alert('ta suave veinho')
+    }
 
+}
