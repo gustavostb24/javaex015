9c9db24bbe855ca071fb23cf27ae6cef234dbada
gustavostb24 <santusbr@hotmail.com>
2025-02-15 19:26:48 -0300

    att

--- Aula 14/ex15/script.js
+++ Aula 14/ex15/script.js
@@ -1,1 +1,17 @@
-alert('oi')
+function gerar() {
+      let num = document.getElementById('inumero')
+      let tab = document.getElementById('itabuada')
+      if (num.value.length == 0) {
+            alert('digite um número')
+      } else {
+            let n = Number(num.value)
+            let c = 1
+            tab.innerHTML=''
+             while (c <= 10) {
+                  let item = document.createElement('option')
+                  item.text = `${n} X ${c} = ${n*c}`
+                  tab.appendChild(item)
+                  c++
+      }
+}
+}
