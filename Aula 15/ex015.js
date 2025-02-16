bd6b0f90fa8752334a6fc6275ae0215d1f39515d
gustavostb24 <santusbr@hotmail.com>
2025-02-16 18:48:39 -0300

    att

---
+++ Aula 15/ex015.js
@@ -0,0 +1,8 @@
+let num = [5,8,2,9,3,4,7]
+num.sort()
+
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+for(let pos=0 ; pos<num.length ; pos++){
+console.log(`A posição ${pos} tem o valor ${num[pos]}`)} 
+console.log(num.indexOf(2))
